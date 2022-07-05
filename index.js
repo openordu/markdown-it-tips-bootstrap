06e01362389e6d079949c3598e107c040ccda184
Chris Godwin <patchshorts@gmail.com>
2022-07-04 19:14:16 -0500

    first edition

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var container = require('markdown-it-container');
+
+module.exports = function md_align_plugin(md, options) {
+	var containerOpenCount = 0;
+	var links = options ? options.links : true;
+	init();
+	return;
+	
+	function setupContainer(name) { 
+        md.use(container, name, {
+            render: function (tokens, idx) {
+                if (tokens[idx].nesting === 1) {
+                    containerOpenCount += 1;
+                    if (name == 'right') {
+                        return '<div class="float-end">\n';
+                    } else if (name == 'left') {
+                        return '<div class="float-start">\n';
+                    } else if (name == 'center') {
+                        return '<div class="">\n';
+                    } else {
+                        return '<div class="alert alert-' + name + '">\n';
+                    }
+                } else {
+                    return '</div>\n';
+                }
+            }
+        });
+    }
+	
+    function isContainerOpen() {
+        return containerOpenCount > 0;
+    }
+	
+    function selfRender(tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+    }
+    
+	function setupLinks() {
+		var defaultRender = md.renderer.rules.link_open || selfRender;
+		
+		md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+			if (isContainerOpen()) {
+				tokens[idx].attrPush(['class', 'md-align-link']);
+			}
+			
+			return defaultRender(tokens, idx, options, env, self);
+		};
+	}
+	
+	function init() {
+        setupContainer('primary');
+        setupContainer('secondary');
+        setupContainer('success');
+        setupContainer('danger');
+        setupContainer('warning');
+        setupContainer('info');
+        setupContainer('light');
+        setupContainer('dark');
+        setupContainer('right');
+        setupContainer('left');
+        setupContainer('center');
+		
+		if (links) {
+			setupLinks();
+		}
+	}
+};
